f3336f2fa011cef4e6286d9b96b157604da3b78c
Emmanuel Ochade <eochade16@gmail.com>
2024-03-19 15:58:05 +0100

    changed w-[100%] to w-full

--- src/app/app/home/page.tsx
+++ src/app/app/home/page.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="w-[100%] flex gap-2">
+    <div className="w-full flex gap-2">
       <div className="w-[34%] flex-col gap-1">
-        <div className="w-[100%] flex justify-between p-2 bg-white rounded-md shadow-md font-semibold">
+        <div className="w-full flex justify-between p-2 bg-white rounded-md shadow-md font-semibold">
           <div className="flex justify-between align-middle gap-2">
             <p>Queue</p>
             <div className="rounded border-r-pink-600">
@@ -17,7 +17,7 @@ const HomePage = () => {
         </div>
       </div>
       <div className="w-[34%] flex-col">
-        <div className="w-[100%] flex justify-between p-2 bg-white rounded-md shadow-md font-semibold">
+        <div className="w-full flex justify-between p-2 bg-white rounded-md shadow-md font-semibold">
           <div className="flex justify-between align-middle gap-2">
             <p>Queue</p>
             <div className="rounded border-r-pink-600">
@@ -28,7 +28,7 @@ const HomePage = () => {
             <p>Queue</p>
           </div>
         </div>
-        <div className="w-[100%] flex gap-2 p-2 bg-white mt-2 rounded-md shadow-md">
+        <div className="w-full flex gap-2 p-2 bg-white mt-2 rounded-md shadow-md">
           <div className="p-4 rounded-md bg-gray-300"></div>
         </div>
       </div>
