fb5cd2831bfcdaf74c6518804b4e782e3982f409
Olajide Shonuga <65728252+jidoggs@users.noreply.github.com>
2024-03-19 10:59:26 +0100

    prettier config

--- src/common/components/private/Layout/index.tsx
+++ src/common/components/private/Layout/index.tsx
@@ -1,10 +1,10 @@
-"use client";
-import React from "react";
-import { Layout as AntLayout } from "antd";
-import { UserPreDefinedRole } from "@/types";
-import { User } from "@/app/auth/types/auth";
-import AppHeader from "../Header";
-import SideNav from "../SideNav";
+'use client';
+import React from 'react';
+import { Layout as AntLayout } from 'antd';
+import { UserPreDefinedRole } from '@/types';
+import { User } from '@/app/auth/types/auth';
+import AppHeader from '../Header';
+import SideNav from '../SideNav';
 
 const { Content } = AntLayout;
 
@@ -16,9 +16,9 @@ type Props = {
 function Layout({ children, user }: Props) {
   return (
     <AntLayout className="!bg-white">
-      <div className="h-screen flex w-full">
-        <SideNav role={user?.["role.name"] as UserPreDefinedRole} />
-        <AntLayout className="flex flex-col h-screen overflow-y-scroll w-full">
+      <div className="flex h-screen w-full">
+        <SideNav role={user?.['role.name'] as UserPreDefinedRole} />
+        <AntLayout className="flex h-screen w-full flex-col overflow-y-scroll">
           <AppHeader />
           <Content className="w-full px-5 pt-6">{children}</Content>
         </AntLayout>
