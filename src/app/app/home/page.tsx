8d3ef8fa959bcb98c5adfe173295c435a30aa0f5
Emmanuel Ochade <eochade16@gmail.com>
2024-03-19 09:04:57 +0100

    added the layout for the home page

--- src/app/app/home/page.tsx
+++ src/app/app/home/page.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div>
-      
+    <div className="w-[100%] flex gap-2">
+      <div className="w-[34%] flex-col gap-1">
+        <div className="w-[100%] flex justify-between p-2 bg-white rounded-md shadow-md font-semibold">
+          <div className="flex justify-between align-middle gap-2">
+            <p>Queue</p>
+            <div className="rounded border-r-pink-600">
+              <p>20</p>
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <p>Queue</p>
+          </div>
+        </div>
+      </div>
+      <div className="w-[34%] flex-col">
+        <div className="w-[100%] flex justify-between p-2 bg-white rounded-md shadow-md font-semibold">
+          <div className="flex justify-between align-middle gap-2">
+            <p>Queue</p>
+            <div className="rounded border-r-pink-600">
+              <p>20</p>
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <p>Queue</p>
+          </div>
+        </div>
+        <div className="w-[100%] flex gap-2 p-2 bg-white mt-2 rounded-md shadow-md">
+          <div className="p-4 rounded-md bg-gray-300"></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
