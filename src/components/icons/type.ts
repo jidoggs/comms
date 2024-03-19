72b08b64651c2a8c2bdf08b7b6de6d3141e2ed5f
Olajide Shonuga <65728252+jidoggs@users.noreply.github.com>
2024-03-19 01:23:55 +0100

    lint test

--- src/components/icons/type.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-interface Props extends React.SVGProps<SVGSVGElement> {
-  size?: string;
-}
-
-export type Icon = React.FunctionComponent<Props>;
---
+++ src/service/config/constant.ts
@@ -0,0 +1,9 @@
+export const BASE_URL = process.env.NEXT_PUBLIC_SERVER_API;
+
+export const REQUEST_TIMEOUT = 60000;
+export const PAGE_SIZE = 10;
+
+export const STORAGE_KEYS = {
+  CLIENT_TOKEN_STORAGE_KEY: "correspondence.token",
+  CLIENT_TOKEN_REFRESH_KEY: "correspondence.refresh_token",
+};
