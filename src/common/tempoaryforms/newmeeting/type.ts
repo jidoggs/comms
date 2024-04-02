0ed2ab2e84c6f1219b9cd2d03d5d2eacab3fa8c9
Olajide Shonuga <65728252+jidoggs@users.noreply.github.com>
2024-04-02 10:17:20 +0100

    form template

---
+++ src/common/tempoaryforms/newmeeting/type.ts
@@ -0,0 +1,9 @@
+export interface CorrespondenceData {
+  sent_by: string;
+  recipient: string;
+  subject: string;
+  ref_no: string;
+  document: string;
+  comment: string;
+  created_at: string;
+}
