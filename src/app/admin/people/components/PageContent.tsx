d1982c79d9e4c3b945e09df4d0b4d064f087bd9c
Emmanuel Ochade <eochade16@gmail.com>
2024-04-19 07:54:12 +0100

    updated the personal details screen

--- src/app/admin/people/components/PageContent.tsx
+++ src/app/admin/people/components/PageContent.tsx
@@ -1,12 +1,26 @@
 'use client';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import CustomTable from '@/common/components/CustomTable';
 import CustomTab from '@/common/components/CustomTab';
 import TableActions from './TableActions';
 import { PeopleDataContext } from '../service-context/PeopleListContextWrapper';
+import CustomModal from '@/common/components/CustomModal';
+import { User } from '@/app/auth/types/auth';
+import RegistrationDetailsModal from './RegistrationDetailsModal';
 
 const CorrespondencePage = () => {
   const contextInfo = useContext(PeopleDataContext);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [staffData, setStaffData] = useState<User>({} as User);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div className="pt-4">
       <CustomTable
@@ -26,7 +40,17 @@ const CorrespondencePage = () => {
         dataSource={contextInfo?.dataSource}
         size="large"
         rowClassName="group"
+        onRow={(record) => ({
+          onClick: () => {
+            setStaffData(record);
+            showModal();
+          },
+          style: { cursor: 'pointer' },
+        })}
       />
+      <CustomModal open={isModalOpen} onCancel={handleCancel} width="80%">
+        <RegistrationDetailsModal registrationData={staffData} />
+      </CustomModal>
     </div>
   );
 };
---
+++ src/app/admin/people/components/RegistrationDetailsModal.tsx
@@ -0,0 +1,91 @@
+import CustomButton from '@/common/components/CustomButton';
+import Title from '@/common/components/Title';
+import { Close, Delete } from '@/common/components/icons';
+import Image from 'next/image';
+import React from 'react';
+import FieldRow from './FieldRow';
+import dayjs from 'dayjs';
+import Tick from '@/common/components/icons/Tick';
+import { User } from '@/app/auth/types/auth';
+
+type RegistrationDetailsModalProps = {
+  registrationData: User;
+};
+
+const RegistrationDetailsModal = ({
+  registrationData,
+}: RegistrationDetailsModalProps) => {
+  return (
+    <div className="flex w-full justify-center pb-8">
+      <div className="w-2/4">
+        <Title className="font-semibold text-custom-gray_600">Personal</Title>
+        <div className="flex gap-2 py-2">
+          <Image
+            width={200}
+            height={100}
+            style={{ objectFit: 'contain' }}
+            src="/images/dummyUserImg.png"
+            alt=""
+          />
+          <div className="flex flex-col justify-end">
+            <div className="flex items-center gap-1">
+              <div className="size-2 rounded bg-custom-green_100" />
+              <p>Active</p>
+            </div>
+            <div className="flex gap-2">
+              <CustomButton
+                type="default"
+                className="!bg-custom-gray_100 !px-6 !text-custom-main"
+                icon={<Delete size={18} />}
+                size="small"
+                title="Delete Profile"
+              />
+              <CustomButton
+                type="default"
+                className="!border-custom-green_100 !bg-custom-gray_100 !px-6"
+                icon={<Tick size={18} color="green" />}
+                size="small"
+                title="Delete Profile"
+              />
+              <CustomButton
+                type="default"
+                className="!border-custom-red_200 !bg-custom-gray_100 !px-6"
+                icon={<Close size={32} color="green" />}
+                size="small"
+                title="Delete Profile"
+              />
+            </div>
+          </div>
+        </div>
+        <hr />
+        <FieldRow title="Name" value={registrationData?.firstname as string} />
+        <FieldRow title="Email" value={registrationData?.email as string} />
+        <FieldRow
+          title="Phone Number"
+          value={registrationData?.phone as string}
+        />
+        <FieldRow
+          title="Date Added"
+          value={dayjs(registrationData?.date_created).format('DD-MMM-YYYY')}
+        />
+        <FieldRow
+          title="Last Active"
+          value={dayjs(registrationData?.date_created).format('DD-MMM-YYYY')}
+        />
+        <div>
+          <Title className="my-2 font-semibold text-custom-gray_600">
+            Office
+          </Title>
+          <FieldRow title="Title" value={registrationData?.title} />
+          <FieldRow title="Department" value={registrationData?.department} />
+          <FieldRow
+            title="Parastatal"
+            value={registrationData?.parastatal as string}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegistrationDetailsModal;
